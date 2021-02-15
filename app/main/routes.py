c7358d576a8d27d03303b7acf32a8e8b8283dcc7
Bas van der Borden <bascasper@gmail.com>
2021-02-15 22:29:04 +0100

    d3 map and location points

--- app/main/routes.py
+++ app/main/routes.py
@@ -36,7 +36,7 @@ def bokeh():
 @main.route("/data", methods=['GET'])
 def get_data():
 	area = request.args.get("area")
-	
+
 	property_type = request.args.get("property")
 	rental_price = request.args.get("price")
 	surface_area = request.args.get("surface")
@@ -59,13 +59,13 @@ def get_data():
 	_, probabilities = models.pred_proba(model=trained_model, input_vars=query_input)
 	proba_idx = np.where(probabilities[0] == np.amax(probabilities[0]))
 	proba_idx = proba_idx[0][0]
-	
+
 	#determine the index of the predicted area within the returned probabiblities array
 	pred_area = data.area_names[proba_idx]
 	proba = probabilities[0][proba_idx]
 	proba = '%.3f' % Decimal(proba)
 
-	#determine how the prediction probability of our previously predicted area has changed 
+	#determine how the prediction probability of our previously predicted area has changed
 	#due to the change in data variables of this area
 	if area is not None:
 		proba_idx = data.area_names.index(area)
@@ -77,18 +77,18 @@ def get_data():
 		plot_area = pred_area
 
 	del probabilities
-		
+
 	if plot is not None:
 		plot_data = data.model_data.loc[data.model_data['area_name'] == plot_area]
 		plot_data = plot_data.loc[:, data.model_vars]
 		plot = plots.create_hbar(plot_area, plot_data)
-		return jsonify(prediction=pred_area, prediction_proba=proba, 
+		return jsonify(prediction=pred_area, prediction_proba=proba,
 				area_changed_proba=new_proba_prev_area, plotData=plot)
 	else:
-		return jsonify(prediction=pred_area, prediction_proba=proba, 
+		return jsonify(prediction=pred_area, prediction_proba=proba,
 				area_changed_proba=new_proba_prev_area)
 
-	
+
 
 
 @main.route('/d3', methods = ['GET'])
@@ -102,9 +102,11 @@ def d3():
 	plot_data = plot_data.drop(['area_name', 'area_code'], axis=1)
 	plot_data = plot_data.to_json(orient='records')
 
+	longlat = [data.stats_ams['Long'], data.stats_ams['Lat'], data.stats_ams['area_name']]
+
 	meta_data = data.stats_ams_meta.to_json(orient='records')
 	return render_template("d3.html", meta_data=meta_data,
-		x_variables=data.model_vars, area_names=data.area_names, selected_area_name=area_name)
+		x_variables=data.model_vars, area_names=data.area_names, selected_area_name=area_name, long_lat=longlat)
 
 
 @main.route('/d3_plot_data', methods = ['GET'])
@@ -116,7 +118,3 @@ def d3_plot_data():
 	plot_data = plot_data.to_json(orient='records')
 
 	return plot_data
-
-
-
-
